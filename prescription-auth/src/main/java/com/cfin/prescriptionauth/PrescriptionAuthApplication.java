182ac85383ba3dd1494e0e10ccf6d5a34b9a78d5
ilie-vlad <vlad.iulian.ilie@gmail.com>
2018-12-19 15:09:35 +0200

    Added postgres Dockerfile and skeleton for auth microservice

---
+++ prescription-auth/src/main/java/com/cfin/prescriptionauth/PrescriptionAuthApplication.java
@@ -0,0 +1,14 @@
+package com.cfin.prescriptionauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescriptionAuthApplication.class, args);
+	}
+
+}
+
---
+++ prescription-auth/src/test/java/com/cfin/prescriptionauth/PrescriptionAuthApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cfin.prescriptionauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrescriptionAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
